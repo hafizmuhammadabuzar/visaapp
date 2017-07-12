e0f75c73568e561fe7d0aedd0d879a86f511b908
Imran <m.imran8811@gmail.com>
2017-07-12 10:15:48 +0500

    Login page and Dashboard Design Ready

---
+++ application/views/admin/login.php
@@ -0,0 +1,34 @@
+<div id="wrapper" class="login">
+
+    <!-- Navigation -->
+    <?php // include 'navigation.php'; ?>
+	<div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+	            	<div class="LoginBox">  
+	            		<h1 class="page-header">Login</h1>
+                        <div class="form-group">
+                            <label>Username:</label>
+                            <input type="text" />
+                        </div>
+                        <div class="form-group">
+                            <label>Password:</label>
+                            <input type="password" />
+                        </div>
+                        <div class="form-group LeftBox">
+	                        <input type="checkbox">
+                            <label>Remember me</label>
+                        </div>
+                        <div class="form-group">
+	                        <input type="submit" value="Log In">
+                        </div>
+                        <div class="clearfix"></div>
+                	</div>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+   </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
